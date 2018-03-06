aa71cd41391aea8c102a23264b5bb03a6aa10e40
Daniel Bowen <danielrbowen@live.com>
2018-03-06 10:30:19 -0700

    Stopwatch. 8.4 MB, 0.0280255 Seconds - Delegate. 8.5 MB, 0.042906 Seconds - Enumerator.

--- ConsoleTests/Program.cs
+++ ConsoleTests/Program.cs
@@ -1,15 +1,21 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 
 namespace ConsoleTests
 {
     class Program
     {
+        // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/statements-expressions-operators/anonymous-functions
         delegate string BuzzFizz0(int index);
 
         static void Main(string[] args)
         {
+            var stopwatch = Stopwatch.StartNew();
+
             var oneToHundred = Enumerable.Range(1, 100).ToList();
+            //CodingExercises.CodingExercises.BuzzFizz(100).ToList().ForEach(Console.WriteLine);
 
             foreach (var number in oneToHundred)
             {
@@ -23,13 +29,14 @@ namespace ConsoleTests
                 }))(number));
             }
 
+            Console.WriteLine(stopwatch.Elapsed.TotalSeconds.ToString(NumberFormatInfo.InvariantInfo));
+
             Console.ReadLine();
         }
 
         public static void Old()
         {
             CodingExercises.CodingExercises.BuzzFizz(100).ToList().ForEach(Console.WriteLine);
-            var oneToHundred = Enumerable.Range(1, 100).ToList();
         }
     }
 }
